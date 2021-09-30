20af4f8ad8083f5635f8539f22a50ddbc1fe025b
Jordan Oetting <joetting71@gmail.com>
2021-09-30 22:03:20 +0000

    working on post

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,29 @@
 const router = require("express").Router();
 const layout = require("../views/layout.js");
 const addPage = require("../views/addPage");
+const { Page } = require("../models");
 
 router.get("/", (req, res) => {
     res.send(layout());
 });
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("this is a post");
+router.post("/", async (req, res, next) => {
+    let body = req.body;
+    
+    try {
+        const page = await Page.create({
+            title: body.title,
+            content: body.content
+        })
+        
+        res.redirect("/");
+    
+        
+    } catch (error){
+        next(error);
+    }
+    
+    
 });
 
 router.get("/add", (req, res) => {
